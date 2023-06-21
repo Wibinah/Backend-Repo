eecf7fced92f16efc9d657ab5478911b0009465e
Wibinah <wibinahtetuh@gmail.com>
2023-06-21 16:53:04 +0100

    first commit

---
+++ config/app.config.js
@@ -0,0 +1,12 @@
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+    // DB: 'mongodb+srv://wibinahtetuh:VzXxndoqPwK0s4o0@cluster0.d16osnz.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+    DB : 'mongodb+srv://wibinahtetuh:VzXxndoqPwK0s4o0@backend-portfolio.sryk3pd.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+}
+
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
